469a2ac816bd4d2be996b4fa47b226bf1b29cfe7
ahmedanwar123 <ahmed.anwar2003@gmail.com>
2024-11-27 13:45:46 +0200

    Merging Branches

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 This project simulates a 2-link robotic manipulator following a predefined path
 using inverse kinematics to calculate the required joint angles at each step.
 The manipulator consists of two links (L1 and L2), and the path is defined by
-the equation y =  mx + b, where x ranges from -90 mm to 90 mm.
+the equation y =  mx + b, where x ranges from -90 to 90.
 
 The system is divided into three main components:
 1. "Manipulator": This class handles the inverse kinematics calculations,
